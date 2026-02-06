119b2aa0a51d3bb6733d111b8ae63d3b10cd65a9
janosmarc2002-pixel <janosmarc2002@gmail.com>
2026-02-06 22:04:08 +0100

    Refactor registration function in auth.js
    Renamed registration function and updated alert messages.

--- auth.js
+++ auth.js
@@ -1,25 +1,9 @@
-const supabaseUrl = "https://qaiiulhqednhemiqetwb.supabase.co";
-const supabaseKey = "sb_publishable_ds_1s_9HSMFSLHTKkC3iGQ_H3ahnCRe";
-
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
-
-async function regisztral(email, jelszo) {
+async function regisztralSupabase(email, jelszo) {
   const { error } = await supabase.auth.signUp({
     email: email,
     password: jelszo
-  })
+  });
 
-  if (error) alert(error.message)
-  else alert("Email kiküldve a megadott címre")
-}
-
-async function belepes(email, jelszo) {
-  const { error } = await supabase.auth.signInWithPassword({
-    email: email,
-    password: jelszo
-  })
-
-  if (error) alert(error.message)
-  else window.location.href = "jatek.html"
+  if (error) alert(error.message);
+  else alert("Email kiküldve — nézd a postaládát");
 }
