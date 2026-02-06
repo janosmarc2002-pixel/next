d1965f74b141ef32c75f8631b61fcbcd37fb2420
janosmarc2002-pixel <janosmarc2002@gmail.com>
2026-02-06 16:55:50 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,25 @@
 const supabaseUrl = "https://qaiiulhqednhemiqetwb.supabase.co";
 const supabaseKey = "sb_publishable_ds_1s_9HSMFSLHTKkC3iGQ_H3ahnCRe";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 async function regisztral(email, jelszo) {
   const { error } = await supabase.auth.signUp({
     email: email,
     password: jelszo
-  });
+  })
 
-  if (error) alert(error.message);
-  else alert("Regisztráció sikeres");
+  if (error) alert(error.message)
+  else alert("Email kiküldve a megadott címre")
 }
 
-async function belep(email, jelszo) {
+async function belepes(email, jelszo) {
   const { error } = await supabase.auth.signInWithPassword({
     email: email,
     password: jelszo
-  });
+  })
 
-  if (error) alert(error.message);
-  else alert("Belépés sikeres");
+  if (error) alert(error.message)
+  else window.location.href = "jatek.html"
 }
